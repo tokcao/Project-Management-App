ca438d48153b764467281be6003d00fbdca99b4d
Tianyu Cao <tianyu.cao@sjsu.edu>
2019-08-20 19:48:34 -0700

    full version

---
+++ dist/precache-manifest.fd14e6fa59342159f8b936d09e50a292.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da692639c45f4ef5676f74e210dd53a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e43e875220ef1e12bf0",
+    "url": "/static/css/main.ed77b6ac.chunk.css"
+  },
+  {
+    "revision": "a68078c162713d16dd3f",
+    "url": "/static/js/2.848aed23.chunk.js"
+  },
+  {
+    "revision": "2e43e875220ef1e12bf0",
+    "url": "/static/js/main.dc807115.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
